43632da5a1d8570bd981d80be37cea3f28c47ca6
Dwij13 <dwijjindal2005@gmail.com>
2024-04-25 15:41:38 +0530

    Complete

---
+++ react-redux/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import "./App.css"
+import LikeCount from './Components/LikeCount';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <LikeCount />
+    </div>
+  );
+};
+
+export default App;
